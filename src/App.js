c806c17765d3c6b106f4d209890c71ff5f99657c
MolnarArthur <molnararthur26@gmail.com>
2017-12-20 13:42:52 +0200

    Images Object

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
 
 let myImages = {
   images: [
-    { firstImage:  "<img src='https://s3.eu-central-1.amazonaws.com/absolutejobs-files-dev/images/2017/12/15/b9de3a7abdb7ac6ba4b3d317.jpeg' alt='Happy New Year - Work hard, party harder'>"},
-    { secondImage: "<img src='https://s3.eu-central-1.amazonaws.com/absolutejobs-files-dev/images/2017/12/15/a4f67c13209e197793bf18ab.jpeg' alt='Nieuwe kantoren'>" },
-    { thirdImage:  "<img src='https://s3.eu-central-1.amazonaws.com/absolutejobs-files-dev/images/2016/12/22/7f8a131245ebd3795e2eb10e.jpeg' alt='photo'>"}
+    { firstImage:  <img src='https://s3.eu-central-1.amazonaws.com/absolutejobs-files-dev/images/2017/12/15/b9de3a7abdb7ac6ba4b3d317.jpeg' alt='Happy New Year - Work hard, party harder'/>},
+    { secondImage: <img src='https://s3.eu-central-1.amazonaws.com/absolutejobs-files-dev/images/2017/12/15/a4f67c13209e197793bf18ab.jpeg' alt='Nieuwe kantoren'/> },
+    { thirdImage:  <img src='https://s3.eu-central-1.amazonaws.com/absolutejobs-files-dev/images/2016/12/22/7f8a131245ebd3795e2eb10e.jpeg' alt='photo'/>}
   ]
 }
 
--- src/ContainerComponent.js
+++ src/ContainerComponent.js
@@ -15,10 +15,13 @@ import FooterComponent from './FooterComponent.js'
 class Container extends Component {
 
   render() {
+    this.props.images.forEach(function (el, i) {
+      console.log(el);
+    });
+
     return (
       <div className="header-section">
           <div className="background">
-
 
             <img className="header-background-image" src="https://s3.eu-central-1.amazonaws.com/absolutejobs-files-dev/images/2017/12/15/b9de3a7abdb7ac6ba4b3d317.jpeg" />
             <HeaderMenuComponent />
